17bfb086985d4e91e766406423717af546a1b299
Idemiron <idemiron@gmail.com>
2022-09-19 08:35:26 -0700

    K

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+/**
+* void peototype()
+* int prototypes()
+*/
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int_strlen(char *s);
+void_puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+int_atoi(char *str);
+
