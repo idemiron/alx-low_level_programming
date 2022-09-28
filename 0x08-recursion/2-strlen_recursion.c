b730ffa609dcfbd7c8ac17c091ccc085e7f0a7f3
Idemiron <idemiron@gmail.com>
2022-09-28 03:11:44 -0700

    twob

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,14 +1,17 @@
 #include "main.h"
+
 /**
- * _print_rev_recursion - Prints a string in reverse, followed by a new line.
- * @s: the string to be printed
+ * _strlen_recursion - Returns Length of String
+ * @s: string
  *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-void _print_rev_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-	if (s[0] != '\0')
+	if (*s != '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(s[0]);
+		return (1 + _strlen_recursion(s + 1));
 	}
+	return (0);
 }
