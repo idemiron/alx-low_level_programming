e01cd97041e1f929f6936bf13faba336b18ee589
Idemiron <idemiron@gmail.com>
2022-10-03 03:11:53 -0700

    One

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
 #include <stdlib.h>
 
 int _putchar(char);
@@ -12,4 +13,4 @@ void free_grid(int **grid, int height);
 char **strtow(char *str);
 char *argstostr(int ac, char **av);
 
-#endif /* MAIN_H */
+#endif
